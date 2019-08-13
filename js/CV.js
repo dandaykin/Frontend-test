f36e008266573db6884457b0df2b03dd40ebf26c
dandaykin <dan.daykin@hotmail.co.uk>
2019-08-13 15:50:49 +0100

    Clock addition

--- js/CV.js
+++ js/CV.js
@@ -1,6 +1,41 @@
+function showtime(){
+    var date = new Date ();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM"
 
+    if (h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = (h - 12);
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m +":" + s + " " + session;
+
+    $('#myclock').text(time);
+
+
+    setTimeout(showtime, 1000);
+
+}
 
 $(document).ready(function() {
+
+
+
+
+    showtime();
+
+
+
     $("p").hover(function() {
         $(this).css("background-color", "white");
     }, function() {
